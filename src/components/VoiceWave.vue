f50e5ab71ece526331e5f4819753c8ff054cf3fb
朱坚 <453424829@qq.com>
2017-12-04 09:54:10 +0800

    修改了聊天室组件

---
+++ src/components/VoiceWave.vue
@@ -0,0 +1,105 @@
+<template>
+    <div  class="vioce-wave-wrapper"> 
+        <span class="voice-icon-bar voice-icon-bar1" :class="{'voice-icon-play1':playId == id && play}"></span>
+        <span class="voice-icon-bar voice-icon-bar2" :class="{'voice-icon-play2':playId == id && play}"></span>
+        <span class="voice-icon-bar voice-icon-bar3" :class="{'voice-icon-play3':playId == id && play}"></span>
+        <span class="voice-icon-bar voice-icon-bar4" :class="{'voice-icon-play4':playId == id && play}"></span>
+        <span class="voice-icon-bar voice-icon-bar5" :class="{'voice-icon-play5':playId == id && play}"></span>
+    </div>
+</template>
+<script>
+export default {
+  name: 'VoiceWave',
+  props: {
+    id:{
+      type: Number,
+      required: true
+    },
+    play:{
+      type: Boolean,
+      default: false
+    },
+    playId: {
+      type: Number,
+      default: -1
+    }
+  },
+  data () {
+    return {
+   
+    }
+  },
+  methods:{
+ 
+  }
+}
+</script>
+<style scoped>
+.vioce-wave-wrapper{
+  align-items: center;
+  display: flex;
+}
+.vioce-wave-wrapper .voice-icon-bar{
+  display: block;
+  width:3px;  
+  margin-left: 3px;  
+  border-radius: 50px;    
+  background-color: #55cbc4;  
+}
+
+@keyframes voicePlay{  
+    0%{  
+        height: 6px;
+    }  
+    20%{  
+        height: 14px;  
+    }  
+    50%{  
+        height: 20px;  
+    }  
+    80%{  
+        height: 14px;      
+    }  
+    100%{  
+        height: 6px;  
+    }  
+}         
+
+.vioce-wave-wrapper .voice-icon-bar1{  
+    margin-left: 10px;
+    height: 6px;
+}  
+.vioce-wave-wrapper .voice-icon-bar2{  
+    height: 14px;
+}  
+.vioce-wave-wrapper .voice-icon-bar3{  
+    height: 20px;
+}  
+.vioce-wave-wrapper .voice-icon-bar4{  
+    height: 14px;
+}  
+.vioce-wave-wrapper .voice-icon-bar5{  
+   margin-right: 10px;
+   height: 6px; 
+}  
+.voice-icon-play1{
+    animation:voicePlay 0.6s infinite 0.1s;  
+   -webkit-animation:voicePlay 0.6s infinite 0.1s;  
+}
+.voice-icon-play2{
+   animation:voicePlay 0.6s infinite 0.2s;  
+   -webkit-animation:voicePlay 0.6s infinite 0.2s;  
+}
+.voice-icon-play3{
+    animation:voicePlay 0.6s infinite 0.3s;  
+   -webkit-animation:voicePlay 0.6s infinite 0.3s;  
+}
+.voice-icon-play4{
+   animation:voicePlay 0.6s infinite 0.4s;  
+   -webkit-animation:voicePlay 0.6s infinite 0.4s;  
+}
+.voice-icon-play5{
+   animation:voicePlay 0.6s infinite 0.5s;  
+   -webkit-animation:voicePlay 0.6s infinite 0.5s; 
+}
+</style>
--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -28,11 +28,7 @@
           <span class="msg-time">2017-7-20 19:20:20</span>
           <img class="user-avatar" src="../../static/timg.jpeg" >
           <div class="chat-content voice-content" @click="playAudio(1)"> 
-             <span class="voice-icon-bar voice-icon-bar1" :class="{'voice-icon-play1':audioPlayId == 1 && audioPlay}"></span>
-             <span class="voice-icon-bar voice-icon-bar2" :class="{'voice-icon-play2':audioPlayId == 1 && audioPlay}"></span>
-             <span class="voice-icon-bar voice-icon-bar3" :class="{'voice-icon-play3':audioPlayId == 1 && audioPlay}"></span>
-             <span class="voice-icon-bar voice-icon-bar4" :class="{'voice-icon-play4':audioPlayId == 1 && audioPlay}"></span>
-             <span class="voice-icon-bar voice-icon-bar5" :class="{'voice-icon-play5':audioPlayId == 1 && audioPlay}"></span>
+             <voice-wave :id="1" :play="audioPlay" :play-id="audioPlayId"></voice-wave>
              <span class="voice-time">6"</span>
           </div>
         </div> 
@@ -41,11 +37,7 @@
           <span class="msg-time">2017-7-20 19:20:20</span>
           <div class="chat-content voice-content" @click="playAudio(2)"> 
              <span class="voice-time">6"</span>
-             <span class="voice-icon-bar voice-icon-bar1" :class="{'voice-icon-play1':audioPlayId == 2 && audioPlay}"></span>
-             <span class="voice-icon-bar voice-icon-bar2" :class="{'voice-icon-play2':audioPlayId == 2 && audioPlay}"></span>
-             <span class="voice-icon-bar voice-icon-bar3" :class="{'voice-icon-play3':audioPlayId == 2 && audioPlay}"></span>
-             <span class="voice-icon-bar voice-icon-bar4" :class="{'voice-icon-play4':audioPlayId == 2 && audioPlay}"></span>
-             <span class="voice-icon-bar voice-icon-bar5" :class="{'voice-icon-play5':audioPlayId == 2 && audioPlay}"></span>
+             <voice-wave :id="2" :play="audioPlay" :play-id="audioPlayId"></voice-wave>
           </div>
           <img class="user-avatar" src="../../static/timg.jpeg">
         </div> 
@@ -105,13 +97,15 @@
 import Scroll  from '../components/Scroll.vue'
 import HeaderNav  from '../components/HeaderNav.vue'
 import CountTimer  from '../components/CountTimer.vue'
+import VoiceWave  from '../components/VoiceWave.vue'
 import T from '../tool/tool'
 export default {
   name: 'ChatRoom',
   components:{
     'header-nav':HeaderNav,
     'v-scroll': Scroll,
-    'count-timer':CountTimer
+    'count-timer':CountTimer,
+    'voice-wave':VoiceWave
   },
    data () {
     return {
@@ -323,7 +317,7 @@ export default {
    border-radius: 50%;
 }
 .chat-msg-wrap .msg-item .chat-content{
-    position: relative;
+  position: relative;
   line-height: 1.5;
   background: #B4EEB4;
   padding: 10px;
@@ -397,74 +391,10 @@ export default {
   to {transform:scale(0.3,0.3) translateY(0%)}
 }
 
-
 .chat-msg-wrap .msg-item .chat-content.voice-content{
   display: flex;
   align-items: center;
 }
-.chat-msg-wrap .msg-item .chat-content.voice-content .voice-icon-bar{
-  width:3px;  
-  margin-left: 3px;  
-  border-radius: 50px;    
-  background-color: #55cbc4;  
-}
-
-@keyframes voicePlay{  
-    0%{  
-        height: 6px;
-    }  
-    20%{  
-        height: 14px;  
-    }  
-    50%{  
-        height: 20px;  
-    }  
-    80%{  
-        height: 14px;      
-    }  
-    100%{  
-        height: 6px;  
-    }  
-}         
-
-.chat-msg-wrap .msg-item .chat-content.voice-content .voice-icon-bar1{  
-    margin-left: 10px;
-    height: 6px;
-}  
-.chat-msg-wrap .msg-item .chat-content.voice-content .voice-icon-bar2{  
-    height: 14px;
-}  
-.chat-msg-wrap .msg-item .chat-content.voice-content .voice-icon-bar3{  
-    height: 20px;
-}  
-.chat-msg-wrap .msg-item .chat-content.voice-content .voice-icon-bar4{  
-    height: 14px;
-}  
-.chat-msg-wrap .msg-item .chat-content.voice-content .voice-icon-bar5{  
-   margin-right: 10px;
-   height: 6px; 
-}  
-.voice-icon-play1{
-    animation:voicePlay 0.6s infinite 0.1s;  
-   -webkit-animation:voicePlay 0.6s infinite 0.1s;  
-}
-.voice-icon-play2{
-   animation:voicePlay 0.6s infinite 0.2s;  
-   -webkit-animation:voicePlay 0.6s infinite 0.2s;  
-}
-.voice-icon-play3{
-    animation:voicePlay 0.6s infinite 0.3s;  
-   -webkit-animation:voicePlay 0.6s infinite 0.3s;  
-}
-.voice-icon-play4{
-   animation:voicePlay 0.6s infinite 0.4s;  
-   -webkit-animation:voicePlay 0.6s infinite 0.4s;  
-}
-.voice-icon-play5{
-   animation:voicePlay 0.6s infinite 0.5s;  
-   -webkit-animation:voicePlay 0.6s infinite 0.5s; 
-}
-
 audio{
   position: absolute;
   visibility: hidden;
@@ -492,7 +422,4 @@ audio{
   border-radius: 4px;
   transform: translateZ(101px);
 }
-
-
-
 </style>
